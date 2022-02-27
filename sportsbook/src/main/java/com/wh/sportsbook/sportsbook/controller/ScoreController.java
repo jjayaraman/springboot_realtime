09fc2ecf743df0b1965ea105e3511074492e92f6
Jayakumar Jayaraman <india.jai@gmail.com>
2022-02-27 09:42:04 +0000

    refactoring

--- sportsbook/src/main/java/com/wh/sportsbook/sportsbook/controller/ScoreController.java
+++ sportsbook/src/main/java/com/wh/sportsbook/sportsbook/controller/ScoreController.java
@@ -1,14 +1,17 @@
 package com.wh.sportsbook.sportsbook.controller;
 
+import com.wh.sportsbook.sportsbook.dto.ScoreDto;
 import com.wh.sportsbook.sportsbook.entity.Score;
 import com.wh.sportsbook.sportsbook.service.ScoreService;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -17,33 +20,46 @@ public class ScoreController {
     @Autowired
     private ScoreService scoreService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping(value = "/score")
-    public ResponseEntity<Score> createScore(@RequestBody Score score) {
-        log.debug("Creating /score record with input : " + score);
+    public ResponseEntity<Score> createScore(@RequestBody ScoreDto scoreDto) {
+        log.debug("Creating /score record with input : " + scoreDto);
+        Score score = convertToEntity(scoreDto);
         Score created = scoreService.createScore(score);
         return ResponseEntity.ok(created);
     }
 
     @GetMapping(value = "/scores", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<List<Score>> getScores() {
+    public ResponseEntity<List<ScoreDto>> getScores() {
         log.debug("Calling /scores api... ");
         List<Score> scores = scoreService.getScores();
-        return ResponseEntity.ok(scores);
+        List<ScoreDto> scoreDtos = scores.stream().map(this::convertToDto).collect(Collectors.toList());
+        return ResponseEntity.ok(scoreDtos);
     }
 
 
     @GetMapping(value = "/score/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Score> getScoreById(@PathVariable Integer id) {
+    public ResponseEntity<ScoreDto> getScoreById(@PathVariable Integer id) {
         log.debug("Calling getScoreById() GET /score/{id} api... ");
         Score score = scoreService.getScoreById(id);
-        return ResponseEntity.ok(score);
+        return ResponseEntity.ok(convertToDto(score));
     }
 
     @PutMapping(value = "/score/{id}")
-    public ResponseEntity<Score> updateScore(@PathVariable Integer id, @RequestBody Score score) {
+    public ResponseEntity<ScoreDto> updateScore(@PathVariable Integer id, @RequestBody Score score) {
         log.debug("Updating /score record with input : " + score + " and the id : " + id);
         Score updated = scoreService.updateScore(id, score);
-        return ResponseEntity.ok(updated);
+        return ResponseEntity.ok(convertToDto(updated));
     }
 
+
+    private Score convertToEntity(ScoreDto scoreDto) {
+        return modelMapper.map(scoreDto, Score.class);
+    }
+
+    private ScoreDto convertToDto(Score score) {
+        return modelMapper.map(score, ScoreDto.class);
+    }
 }
---
+++ sportsbook/src/main/java/com/wh/sportsbook/sportsbook/dto/ScoreDto.java
@@ -0,0 +1,13 @@
+package com.wh.sportsbook.sportsbook.dto;
+
+import lombok.Data;
+
+@Data
+public class ScoreDto {
+
+    private Integer id;
+
+    private String team;
+
+    private Integer score;
+}
